86c87c76ee979c5e275ff063ef56dfaec418aa93
dibikapandey00 <dibikapandey25@gmail.com>
2024-07-16 22:31:13 +0545

    Add files via upload

---
+++ Question no 2.cpp
@@ -0,0 +1,40 @@
+// Q.No.3.  Verify the Modus Ponens using the truth table.
+
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+	int p[4]={1,1,0,0},
+	q[4]={1,0,1,0},
+     a[4],m[4],n[4];
+	for(int i=0;i<4;i++){
+		m[i]=!p[i];
+	}
+	for(int i=0;i<4;i++){
+		n[i]=!q[i];
+	}
+	for(int i=0;i<=4;i++){
+		if(p[i]==1&&q[i]==0){
+			a[i]=0;
+		}
+		else{
+			
+			a[i]=1;
+		}
+	}
+	cout<<"---------------------------------------------------"<<endl;
+	cout<<"|p  |"<<"\t"<<"q  |"<<"\t"<<"(!p)"<<"\t|"<<"  (!q)"<<"  |"<<"(p->q)\t  |"<<endl;
+	cout<<"---------------------------------------------------"<<endl;
+	for(int i=0;i<4;i++){
+		cout<<"|"<<p[i]<<"  |"<<"\t"<<q[i]<<"  |"<<"\t"<<" "<<m[i]<<"\t|"<<" "<<n[i]<<"\t"<<" |"<<"\t"<<a[i]<<"\t"<<"  |"<<endl;
+	}
+		cout<<"---------------------------------------------------"<<endl;
+	for(int i=0;i<4;i++){
+		if(a[i]==1&&n[i]==1&&m[i]==1){
+			cout<<"If p->q is true and p is also true then q is also true(Modus Ponens)";
+		}
+	}
+	return 0;
+}
